286f9eec62807332e38f9b34a5cd69fdab85f524
Alex Ryse <aryse54@gmail.com>
2025-03-26 21:57:40 -0700

    First commit

---
+++ mainwindow.cpp
@@ -0,0 +1,21 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    connect(ui->pushButton, &QPushButton::clicked, this, &MainWindow::onPushButtonClicked);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::onPushButtonClicked()
+{
+    ui->label->setText("Button Clicked!");
+}
